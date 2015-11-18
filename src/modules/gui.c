dd4a008bf344d034a31e4abb85df83d1630e3729
PatrikSundin <patsu326@student.liu.se>
2015-11-18 12:58:54 +0100

    Update gui.c

--- src/modules/gui.c
+++ src/modules/gui.c
@@ -38,7 +38,60 @@ void Gui_Deinit(void)
 {
 
 }
+void Gui_setage(uint8_t age)
+{
+    agestring = sprintf(str, "%d\n", (int)age);
+    Graphics_FillRect(&graphObj, 0, 97, 50, 50, s_GRAPHICS_COLOR__WHITE);
+    Graphics_DrawText(&graphObj, 0, 97, agestring, appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__CENTER, s_GRAPHICS_COLOR__BLACK);
+    SharpLcd_DisplayBuffer(graphObj.pBuf);
+}
 
+void Gui_UpdateValues(uint8_t Refresh, uint8_t Pulse, uint8_t Percentage, uint8_t SpO2)
+{
+    Pulsestring = sprintf(str, "%d\n", (int)Pulse);
+    Percentagestring = sprintf(str, "%d\n", (int)Percentage);
+    SpO2string = sprintf(str, "%d\n", (int)SpO2);
+    
+    if Refresh==0
+    {
+        	Graphics_ClearBuffer(&graphObj, s_GRAPHICS_COLOR__WHITE);
+            Graphics_FillRect(&graphObj, 0, 0, s_GRAPHICS__LCD_WIDTH, 30, s_GRAPHICS_COLOR__WHITE);
+            Graphics_DrawText(&graphObj, 0, 7, (uint8_t *)"blueoxi", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__CENTER, s_GRAPHICS_COLOR__WHITE);
+            Graphics_DrawText(&graphObj, 90, 51, Percentagestring, appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__RIGHT, s_GRAPHICS_COLOR__BLACK);
+            Graphics_DrawText(&graphObj, 90, 90, Pulsestring, appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__RIGHT, s_GRAPHICS_COLOR__BLACK);
+            Graphics_DrawText(&graphObj, 90, 129, SpO2, appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__RIGHT, s_GRAPHICS_COLOR__BLACK);
+            Graphics_DrawText(&graphObj, s_GRAPHICS__LCD_WIDTH-75, 53, (uint8_t *)"% HR", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__LEFT, s_GRAPHICS_COLOR__BLACK);
+            Graphics_DrawText(&graphObj, s_GRAPHICS__LCD_WIDTH-75, 92, (uint8_t *)"BPM", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__LEFT, s_GRAPHICS_COLOR__BLACK);
+            Graphics_DrawText(&graphObj, s_GRAPHICS__LCD_WIDTH-75, 131, (uint8_t *)"% SpO2", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__LEFT, s_GRAPHICS_COLOR__BLACK);
+            SharpLcd_DisplayBuffer(graphObj.pBuf);
+    }
+    else
+    {
+        Graphics_FillRect(&graphObj, 90, 51, 25, 100, s_GRAPHICS_COLOR__WHITE);
+        Graphics_DrawText(&graphObj, 90, 51, Percentagestring, appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__RIGHT, s_GRAPHICS_COLOR__BLACK);
+        Graphics_DrawText(&graphObj, 90, 90, Pulsestring, appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__RIGHT, s_GRAPHICS_COLOR__BLACK);
+        Graphics_DrawText(&graphObj, 90, 129, SpO2, appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__RIGHT, s_GRAPHICS_COLOR__BLACK);
+        SharpLcd_DisplayBuffer(graphObj.pBuf);
+    }
+}
+
+Gui_Endscreen(uint8_t MeanPulse, uint8_t MeanSpO2)
+{
+    MeanPulseString = sprintf(str, "%d\n", (int)MeanPulse);
+    MeanSpO2String = sprintf(str, "%d\n", (int)MeanSpO2);
+    Graphics_ClearBuffer(&graphObj, s_GRAPHICS_COLOR__WHITE);
+    Graphics_ClearBuffer(&graphObj, s_GRAPHICS_COLOR__WHITE);
+	Graphics_FillRect(&graphObj, 0, 0, s_GRAPHICS__LCD_WIDTH, 30, s_GRAPHICS_COLOR__BLACK);
+	Graphics_DrawText(&graphObj, 0, 7, (uint8_t *)"blueoxi", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__CENTER, s_GRAPHICS_COLOR__WHITE);
+	Graphics_DrawText(&graphObj, 0, 50, (uint8_t *)"Mean HR", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__CENTER, s_GRAPHICS_COLOR__BLACK);
+	Graphics_DrawText(&graphObj, 0, 106, (uint8_t *)"Mean O2-sat", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__CENTER, s_GRAPHICS_COLOR__BLACK);
+	Graphics_DrawText(&graphObj, 90, 73, MeanPulseString, appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__CENTER, s_GRAPHICS_COLOR__BLACK);
+	Graphics_DrawText(&graphObj, 90, 129, MeanSpO2String , appleLiGothic_22ptFontInfo, s_GRAPHICS_TEXT_ALIGN__CENTER, s_GRAPHICS_COLOR__BLACK);
+	Graphics_DrawText(&graphObj, s_GRAPHICS__LCD_WIDTH-75, 75, (uint8_t *)"BMP", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__LEFT, s_GRAPHICS_COLOR__BLACK);
+	Graphics_DrawText(&graphObj, s_GRAPHICS__LCD_WIDTH-75, 136, (uint8_t *)"%", appleLiGothic_16ptFontInfo, s_GRAPHICS_TEXT_ALIGN__LEFT, s_GRAPHICS_COLOR__BLACK);
+	SharpLcd_DisplayBuffer(graphObj.pBuf);
+    
+}
 
 /*
 	// Second, write in the age.
